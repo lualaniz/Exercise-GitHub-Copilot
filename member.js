49343c4655c89333fd06dfabec4cffd45baf84ff
lualaniz <lalanizgarcia@palo-it.com>
2025-02-11 04:29:48 +0000

    Copilot second commit Lulu

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 26,
+        skills: ['HTML', 'CSS', 'JS'],
+        address: {
+            city: 'San Francisco',
+            country: 'USA'
+        }
+    };
+
+    return member;
+}
